4cc1fd01e0401d1e87dd6356b742dbb1de8c089a
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-11-05 13:03:50 -0800

    Adding pipeline

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const Pipeline = require('./lib/Pipeline')
+const PipelineStage = require('./lib/PipelineStage')
+const PipelineGate = require('./lib/PipelineGate')
+
 const OpenShiftClient = require('./lib/OpenShiftClient')
 const OpenShiftClientX = require('./lib/OpenShiftClientX')
 
+PipelineStage.prototype.gate = PipelineGate.create
+
 module.exports = {
   Pipeline : Pipeline,
+  PipelineStage : PipelineStage,
+  PipelineGate : PipelineGate,
   OpenShiftClient : OpenShiftClient,
   OpenShiftClientX : OpenShiftClientX
 }
