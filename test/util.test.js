f939cb2abff3d3ee6b07a0f11c5abb28de07d49d
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2019-02-06 11:09:49 -0800

    Fixing build caching bug

--- test/util.test.js
+++ test/util.test.js
@@ -22,4 +22,7 @@ describe('util', function() {
     expect(util.hashString('Hello World')).toEqual('557db03de997c86a4a028e1ebd3a1ceb225be238')
     expect(util.hashString({message:'Hello World'})).toEqual('6c0c73c8fdef129c899271509441773cea232ef6')
   }) //end it
+  it('execSync', function() {
+    expect(util.execSync('git', ['version'], {cwd:'/tmp', encoding:'utf-8'}).status).toEqual(0)
+  })
 }) //end describe
